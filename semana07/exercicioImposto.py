d29d763462904bd3263fdecb8c51385178b823fe
YuriGG_ <94704228+owYuriGG@users.noreply.github.com>
2023-05-15 15:10:41 -0300

    Update exercicioImposto.py

--- semana07/exercicioImposto.py
+++ semana07/exercicioImposto.py
@@ -2,5 +2,8 @@ def somaImposto(taxaImposto, custo):
     custo = ((taxaImposto / 100) * custo) + custo
     return custo
 
-custo = somaImposto(50, 100)
-print(custo)
+def main():
+    custo = somaImposto(50, 100)
+    print(custo)
+
+main()
