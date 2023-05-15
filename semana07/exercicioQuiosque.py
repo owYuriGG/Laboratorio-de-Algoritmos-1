395a78fe49532cbf51cc25f832c3d71766654a63
YuriGG_ <94704228+owYuriGG@users.noreply.github.com>
2023-05-15 15:11:20 -0300

    Update exercicioQuiosque.py

--- semana07/exercicioQuiosque.py
+++ semana07/exercicioQuiosque.py
@@ -6,8 +6,10 @@ def valor(laranjas):
         preco = laranjas * 0.40
     return preco
 
-#Código principal
+def main():
+    laranjas = int(input("Digite a quantia de laranjas compradas: "))
+    preco = (valor(laranjas))
+    print(f"O preço da compra ficou: R$ {preco} reais.")
 
-laranjas = int(input("Digite a quantia de laranjas compradas: "))
-preco = (valor(laranjas))
-print(f"O preço da compra ficou: R$ {preco} reais.")
+#Código principal
+main()
